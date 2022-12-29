edd1d883e5a913c1b6d67f945c0f252904fb36a7
Sheik Mohamed Ali <103345551+sheikmohamedali98@users.noreply.github.com>
2022-12-29 09:17:33 +0530

    Daily Problem Solving

--- src/main/kotlin/Array/MaximumBagsWithFullCapacityofRocks.kt
+++ src/main/kotlin/Array/MaximumBagsWithFullCapacityofRocks.kt
@@ -1,31 +1,55 @@
 package Array
 
 class MaximumBagsWithFullCapacityofRocks {
-    fun maximumBags(capacity: IntArray, rocks: IntArray, additionalRocks: Int):Int {
+    fun maximumBags(capacity: IntArray, rocks: IntArray, additionalRocks: Int): Int {
         var extraRock = additionalRocks
-        var numberOfBagFull  = 0
+        var numberOfBagFull = 0
 
-        for(i in  0 until capacity.size){
-            if(capacity[i] != rocks[i]){
-                val  balanceRock = Math.subtractExact(capacity[i],rocks[i])
-                if(extraRock>=balanceRock && balanceRock!=0) {
-                    rocks[i]+=balanceRock
+        for (i in 0 until capacity.size) {
+            if (capacity[i] != rocks[i]) {
+                val balanceRock = Math.subtractExact(capacity[i], rocks[i])
+                if (extraRock >= balanceRock && balanceRock != 0) {
+                    rocks[i] += balanceRock
                     extraRock = extraRock - balanceRock
                 }
                 println(rocks.contentToString())
             }
-            if(capacity[i] == rocks[i]){
+            if (capacity[i] == rocks[i]) {
                 numberOfBagFull++
             }
         }
         return numberOfBagFull
     }
+
+    fun solutionFromLeetcode(capacity: IntArray, rocks: IntArray, additionalRocks: Int): Int {
+        var extraRock = additionalRocks
+        var numberOfBagFull = 0
+
+        for (i in 0 until capacity.size) {
+            capacity[i] -= rocks[i]
+            if (capacity[i] == 0)
+                numberOfBagFull++
+        }
+        capacity.sort()
+        for (i in 0 until capacity.size) {
+            if (extraRock >= capacity[i] && capacity[i] > 0) {
+                extraRock -= capacity[i]
+                numberOfBagFull++
+            }
+            if (extraRock <= 0 || extraRock < capacity[i])
+                break
+        }
+        return numberOfBagFull
+    }
 }
 
-fun main(){
-    val capacity = intArrayOf(10,2,2)
-    val rock = intArrayOf(2,2,0)
+fun main() {
+    val capacity = intArrayOf(10, 2, 2)
+    val rock = intArrayOf(2, 2, 0)
+    val additionalRocks = 100
 
     val maximumBagsWithFullCapacityofRocks = MaximumBagsWithFullCapacityofRocks()
-    println(maximumBagsWithFullCapacityofRocks.maximumBags(capacity,rock,100))
+    println(maximumBagsWithFullCapacityofRocks.maximumBags(capacity, rock, 100))
+    println(maximumBagsWithFullCapacityofRocks.solutionFromLeetcode(capacity,rock,additionalRocks))
+
 }
--- src/main/kotlin/String/LongestPalindromicSubstring.kt
+++ src/main/kotlin/String/LongestPalindromicSubstring.kt
@@ -1,31 +1,43 @@
 package String
 
+import java.util.Stack
+
 class LongestPalindromicSubstring {
     var max = 0
-    fun longestPalindrome(s: String) {
-        for(i in 2 .. s.length){
-            val word = s.subSequence(0 ,i)
-            if(checkPalindrom(word.toString())){
-                max = maxOf(max,word.length)
+    var longestPolindrome =""
+    val stacks = Stack<Int>()
+    fun longestPalindrome(s: String):Int {
+        stacks.push(0)
+        for (i in 0..s.length - 2) {
+            for (j in i + 2..s.length) {
+                val word = s.subSequence(i, j)
+                if (checkPalindrom(word.toString())) {
+                    val maxlength = word.length
+                    if(maxlength>stacks.peek()||stacks.isEmpty()) {
+                        stacks.pop()
+                        stacks.push(word.length)
+                    }
+                }
             }
-            println(word)
         }
-println(max)
+       return stacks.peek()
     }
-    fun checkPalindrom(string: String):Boolean{
-        var start = 0
-        var end = string.length-1
 
-        while (start<=end){
-            if(string[start++].toChar()!= string[end--].toChar()){
+    fun checkPalindrom(string: String): Boolean {
+        var start = 0
+        var end = string.length - 1
+
+        while (start <= end) {
+            if (string[start++].toChar() != string[end--].toChar()) {
                 return false
             }
         }
         return true
     }
 }
-fun main(){
+
+fun main() {
     val longestPalindromicSubstring = LongestPalindromicSubstring()
     println(longestPalindromicSubstring.checkPalindrom("mada"))
-    longestPalindromicSubstring.longestPalindrome("cbbd")
+   println( longestPalindromicSubstring.longestPalindrome("madam"))
 }
